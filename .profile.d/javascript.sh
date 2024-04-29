0258ba0686a7599e168f9f211d3e6ccbe09d47ab
Caleb Hailey <github@calebhailey.com>
2024-04-29 13:33:27 -0700

    updated javascript.sh config and .npmrc

--- .profile.d/javascript.sh
+++ .profile.d/javascript.sh
@@ -1,9 +1,25 @@
-# NodeJS 
+# Javascript Runtimes
+# 
+# NodeJS, Deno, Bun, and future WinterCG runtimes? 🤞
+
+# NodeJS + NPM 
 # 
 # installed via: https://nodejs.org/en/download/current
 # config file: ${HOME}/.npmrc
+# 
+# add npm install -g location to PATH
+if [ -x $(command -v npm) ]; then
+    PATH="$(npm config get prefix)/bin:${PATH}"
+fi;
 
-# add node install -g location to path
-if [ -d "$HOME/.nodejs/bin" ]; then
-    PATH="$HOME/.nodejs/bin:$PATH"
+# Deno
+# 
+# install via: https://docs.deno.com/runtime/manual/getting_started/installation
+# see deno --help for more information on DENO_ environment variables
+# 
+# set DENO_INSTALL_ROOT and add it to PATH
+if [ -x $(command -v deno) ]; then
+    mkdir -p ${HOME}/.deno/bin
+    export DENO_INSTALL_ROOT="${HOME}/.deno/bin"
+    PATH="${DENO_INSTALL_ROOT}:${PATH}"
 fi;
