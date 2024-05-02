78f87c48ef1317c699ec590f8354314fc8189c9c
Caleb Hailey <github@calebhailey.com>
2024-05-02 15:56:25 -0700

    add container registries.conf, update dotfiles.sh

--- .profile.d/podman.sh
+++ .profile.d/podman.sh
@@ -5,4 +5,7 @@
 if [ -d "/opt/podman" ]; then
     ln -Ffs /opt/podman ${HOME}/.podman 2> /dev/null
     PATH="${HOME}/.podman/bin:${PATH}"
+    export KIND_EXPERIMENTAL_PROVIDER=podman
+    podman machine ssh rm -r /home/core/.config/containers
+    podman machine ssh ln -Ffs /Users/calebhailey/.config/containers /home/core/.config/containers 
 fi;
