e076822426582e65473bb313027d268104866989
Caleb Hailey <github@calebhailey.com>
2024-06-09 09:10:59 -0700

    updated git, gitignore, podman, and new passphrase alias

--- .profile.d/dotfiles.sh
+++ .profile.d/dotfiles.sh
@@ -11,7 +11,7 @@ alias dotfiles="/usr/bin/git --git-dir=${HOME}/.dotfiles --work-tree=${HOME}/"
 # revert with `dotfiles update-index --no-skip-worktree <file>`
 # observe with `dotfiles ls-files -v`
 # add changes with `dotfiles add <file>` as normal
-dotfiles update-index --skip-worktree .ssh/config
-dotfiles update-index --skip-worktree .gitconfig
-dotfiles update-index --skip-worktree .config/containers/registries.conf
+dotfiles update-index --skip-worktree "${HOME}/.ssh/config"
+dotfiles update-index --skip-worktree "${HOME}/.gitconfig"
+dotfiles update-index --skip-worktree "${HOME}/.config/containers/registries.conf"
 
--- .profile.d/podman.sh
+++ .profile.d/podman.sh
@@ -2,10 +2,27 @@
 #
 # installed via: https://podman.io/docs/installation
 # removed installed file at: /etc/paths.d/podman-pkg 
+# 
+# Podman machine config:
+# podman machine init --now
+# podman machine ssh rm -r /home/core/.config/containers
+# podman machine ssh ln -Ffs ${HOME}/.config/containers /home/core/.config/containers
+#
+# Podman rootless config:
+# podman machine ssh
+# printf "$USER:1001:9001000\n" | sudo tee /etc/subuid | sudo tee /etc/subgid
+# podman system migrate
+#
+# Podman registry config:
+# podman volume create registry
+# podman container create --name registry -p 5001:5000 --volume registry:/var/lib/registry/ registry:2
+# podman start registry
 if [ -d "/opt/podman" ]; then
     ln -Ffs /opt/podman ${HOME}/.podman 2> /dev/null
     PATH="${HOME}/.podman/bin:${PATH}"
     export KIND_EXPERIMENTAL_PROVIDER=podman
     podman machine ssh rm -r /home/core/.config/containers
-    podman machine ssh ln -Ffs /Users/calebhailey/.config/containers /home/core/.config/containers 
+    podman machine ssh ln -Ffs ${HOME}/.config/containers /home/core/.config/containers 
+    # podman machine ssh rm -r /home/core/.local/containers
+    # podman machine ssh ln -Ffs ${HOME}/.local/containers
 fi;
