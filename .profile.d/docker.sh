c3136707baf4cda886d73f4fbc1d588f1fe0f12e
Caleb Hailey <github@calebhailey.com>
2024-10-09 10:45:39 -0700

    docker-credentials helper config and comments

--- .profile.d/docker.sh
+++ .profile.d/docker.sh
@@ -6,6 +6,8 @@
 # * https://github.com/docker/compose/releases
 # * https://github.com/docker/docker-credential-helpers/releases
 # 
+# configure credential helpers at ~/.docker/config.json; set "credsStore" to "osxkeychain" or "pass" (not "desktop")
+#
 # docker remote API
 # https://success.docker.com/article/how-do-i-enable-the-remote-api-for-dockerd
 #if [ "$(curl --no-buffer -XGET -s -I -f http://127.0.0.1:2376/containers/json)" ]; then 
